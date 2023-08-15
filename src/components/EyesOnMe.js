e8d0eb4569f0cc131aff7c240c4a956ee007caff
Andrew <a.oppman@gmail.com>
2023-08-14 22:40:52 -0400

    components and events added.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,21 @@
-// Code EyesOnMe Component Here
+//function
+
+function focus() {
+  console.log("Good!");
+}
+
+function blur() {
+  console.log("Hey! Eyes on me!");
+}
+
+function EyesOnMe() {
+  return (
+    <>
+      <button onFocus={focus} onBlur={blur}>
+        Eyes on me
+      </button>
+    </>
+  );
+}
+
+export default EyesOnMe;
